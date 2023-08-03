8292f9e36d3e96bf6a6f0e1545d76f61ec504862
Mountain Lover <pnuskgh@gmail.com>
2023-08-03 16:28:17 +0900

    docs: update cnn

--- laboratory/pnuskgh/model_base.py
+++ laboratory/pnuskgh/model_base.py
@@ -13,6 +13,7 @@
 #--- python  laboratory/pnuskgh/mnist_dense.py
 
 import os
+import pandas as pd
 import signal
 import subprocess
 import webbrowser
@@ -20,11 +21,12 @@ from datetime import datetime
 from tensorflow import keras
 
 def SignalHandler_SIGINT(SignalNumber, Frame):
-     print(' ')
-     
+     print(' sigint')
+
 class MODEL_BASE:
     def __init__(self):
         self.name = 'model_base'
+        self.datetimeFr = datetime.now()
         self.initialize()
         
     def initialize(self):
@@ -36,7 +38,7 @@ class MODEL_BASE:
         if (os.path.exists(f"{self.foldername}/save") == False):
             os.makedirs(f"{self.foldername}/save")
         self.tensorboard_folder = f'{self.foldername}/logs/{self.name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
-        
+    
     def _getName(self, name = None):
         if (name):
             return name
@@ -69,3 +71,33 @@ class MODEL_BASE:
     def run_tensorboard(self):
         webbrowser.open('http://localhost:6006/')
         subprocess.call([ 'tensorboard', f'--logdir={self.tensorboard_folder}', '--host=localhost', '--port=6006' ])
+
+    def print_weights(self, model):
+        weights = model.weights
+        for weight in weights:
+            print(weight)
+            print(' ')
+            
+    def plot(self, model):
+        keras.utils.plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
+        
+    #--- https://deepinout.com/pandas/pandas-questions/786_pandas_hdf5_concurrency_compression_io_performance.html
+    def print_hd5(self, filename = './save/mnist_dense.h5'):
+        store = pd.HDFStore(filename, 'r')
+        store.open('r')
+        # keys = store.keys('native')
+        keys = [
+            n._v_pathname for n in store._handle.walk_nodes("/", "Array")
+        ]
+        for key in keys:
+            node = store.get_node(key)
+            print(node)
+            
+            # print(store.get(key))
+            # print(store.select(key))
+
+            # print(node.__dict__)
+            # print(getattr(node, "atom", None))
+            # print(getattr(node, "_v_attrs", None))
+
+        store.close()
